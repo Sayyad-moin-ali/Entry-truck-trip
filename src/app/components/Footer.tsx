f34797ad365078c578fe38045ec82e5036062efc
sayyad-moin-ali <alisayyadmoin@gmail.com>
2024-08-24 10:29:44 +0530

    fixed code

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -11,13 +11,12 @@ const Footer = () => {
                 <button className=' hover:bg-green-600 hover:text-white bg-amber-300 my-4 rounded-md text-black font-bold px-6 py-[3px]'>Try for Free</button>
                 <p>{footerData.address}</p>
                 <p>{footerData.state}</p>
-
             </div>
 
             <div className={styles.containerWrapper}>
                 <h1 className={styles.footerHeading}>{footerData.connect}</h1>
                 <p>{footerData.contact}</p>
-                <a className={styles.email}  href={`mailto:${footerData.email}`}> {footerData.email}</a>
+                <a className={styles.email} href={`mailto:${footerData.email}`}> {footerData.email}</a>
 
 
             </div>
